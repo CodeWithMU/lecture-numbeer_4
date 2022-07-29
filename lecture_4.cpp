d96214f624ff9776b014d4ee8963e0ef89d5440a
CodeWithMU <110249826+CodeWithMU@users.noreply.github.com>
2022-07-29 21:58:47 +0500

    Add files via upload

---
+++ lecture_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 5;
+    float num2 = 7.5;
+    char alphabet = 'w';
+    string name = "my name is usama";
+    bool value1 = true;
+    bool value2 = false;
+    
+    cout << "this is integer data type: " << num << endl;
+    cout << "this is float data type: " << num2 << endl;
+    cout << "this is character data type :" << alphabet << endl;
+    cout << "this is string data type :" << name << endl;
+    cout << "this bool data type: " << value1 << endl;
+    cout << "this bool data type: " << value2 << endl;
+    cout << "hello" << endl;
+}
